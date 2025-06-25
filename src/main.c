358830e941bd7a072239eacdffb1cd69c0332845
derekmaeshiro <derekmaeshiro14@gmail.com>
2025-06-24 19:24:18 -0700

    feat(drivers): prepare for IO implementation

--- src/main.c
+++ src/main.c
@@ -1,10 +1,20 @@
-#include "drivers/uart.h"
-#include "drivers/i2c.h"
-#include "app/drive.h"
-#include "app/enemy.h"
 #include <msp430.h>
+
+static void test_blink_led(void)
+{
+    // TODO: Use io functions
+    P1DIR |= BIT0;
+    volatile unsigned long int i; // volatile to prevent optimization
+    while (1) {
+        P1OUT ^= BIT0;
+        for (i = 100000; i > 0; i--) { } // delay
+    }
+}
 
 int main(void)
 {
+    // TODO: Move to mcu_init
+    WDTCTL = WDTPW + WDTHOLD;
+    test_blink_led();
     return 0;
 }
